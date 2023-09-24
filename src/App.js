d3feb585dca01fd57dfea7e16fabbf0fe97bd1a9
Prajjwal98Dubey <prajjwaldubey003@gmail.com>
2023-09-24 23:51:13 +0530

    added some animations

--- src/App.js
+++ src/App.js
@@ -1,83 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
 import { useState } from 'react';
-import { FaCloudflare, FaHome, FaTemperatureHigh, FaTemperatureLow, FaTint, FaWind, IconName } from "react-icons/fa";
-
+import images from './images'
 function App() {
-  const [current, setCurrent] = useState({})
-  const [location, setLocation] = useState({})
-  const [text, setText] = useState({})
-  const [term, setTerm] = useState()
-  const [error, setError] = useState(false)
-
-  const getWeather = (searched) => {
-    const options = {
-      method: 'GET',
-      url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-      params: { q: `${searched}` },
-      headers: {
-        'X-RapidAPI-Key': 'f3cf1ed102msh62d85265071397ap1d76c3jsn12c38859b41b',
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-      }
-    };
-
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-      setCurrent(response.data.current)
-      setLocation(response.data.location)
-      setText(response.data.current.condition)
-    }).catch(function (error) {
-      console.error(error);
-      setError(true)
-    });
-  }
   return (
     <>
-      <div className='text-4xl mt-7 text-center'>Weather Report</div>
-      <div className='h-fit'>
-        <input className=' w-1/2 h-[35px] border mt-36 mb-16 ml-[350px] text-center text-2xl border-black rounded-l-lg' placeholder='Enter the City Name' type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
-        <button className='border border-black h-[35px] mb-6 pt-[6px] w-[40px] rounded-r-lg' onClick={() => getWeather(term)}>🔎</button>
-      </div>
-      {error ? <div className='text-3xl text-center'> No Result found for this Search</div> : <div className='mt-5'>
-        <div className='flex p-2 m-5 justify-evenly'>
-          <div className='text-center p-2 items-center flex flex-col border shadow-lg w-[250px] h-[100px]'>
-            <h1 className='text-2xl'>Country</h1>
-            <div className='p-2'><FaHome /></div>
-            <div>{location.country}</div>
-          </div>
-          <div className='text-center items-center p-2  flex flex-col border shadow-lg w-[250px] h-[100px]'>
-            <h1 className='text-2xl'>Temperature</h1>
-            <div className='p-2'><FaTemperatureLow /></div>
-            <div>{current.temp_c}°C</div>
-          </div>
-          <div className='text-center p-2 items-center flex flex-col border shadow-lg w-[250px] h-[100px]'>
-            <h1 className='text-2xl'>Wind Speed</h1>
-            <div className='p-2'><FaWind /></div>
-            <div>{current.wind_kph}km/hr</div>
-          </div>
-        </div>
-        <div className='flex p-2 m-5 justify-evenly'>
-          <div className='text-center p-2 items-center border flex flex-col shadow-lg w-[250px] h-[100px]'>
-            <h1 className='text-2xl'>Feels like</h1>
-            <div className='p-2'><FaTemperatureHigh /></div>
-            <div>{current.feelslike_c}°C</div>
-          </div>
-          <div className='text-center p-2 items-center flex flex-col border shadow-lg w-[250px] h-[100px]'>
-            <h1 className='text-2xl'>Humidity</h1>
-            <div className='p-2'><FaTint /></div>
-            <div>{current.humidity}</div>
-          </div>
-          <div className='text-center p-2 items-center flex flex-col border shadow-lg w-[250px] h-[100px]'>
-            <h1 className='text-2xl'>Forecast</h1>
-            <div className='p-2'><FaCloudflare /></div>
-            <div>{text.text}</div>
-          </div>
-        </div>
-      </div>}
-
+     <SlideShow images={images}/>
     </>
   );
 }
+
+const SlideShow =({images})=>{
+  const[active,setActive]=useState(1)
+  const nextImage=()=>{
+    if (active < images.length-1){
+          setActive(active+1)
+    }
+  }
+  const prevImage=()=>{
+    if (active > 0){
+      setActive(active-1)
+    }
+  }
+return (
+  <>
+  <div className='flex justify-center w-full mt-14 '>
+    <div className='relative'>
+    {images.map((image,i)=><Slide key={image.captions} active={i===active} image_url={image.image_url} />)}
+    <div className='flex justify-center m-2'>
+      {images.map((image,i)=><div key={image.captions} className={`w-[35px] h-[35px] rounded-full bg-gray-500 m-1 cursor-pointer ${active===i ? "bg-blue-600":""}`} onClick={()=>setActive(i)} ></div>)}
+    </div>
+    <div className='text-5xl absolute top-28 right-0 cursor-pointer text-white' onClick={()=>nextImage()}>&gt;</div>
+    <div className='text-5xl absolute top-28 left-0 text-white cursor-pointer' onClick={()=>prevImage()}>&lt;</div>
+  </div>
+  </div>
+  </>
+)
+}
+const Slide=({image_url,active})=>{
+  return (
+  <>
+    <img className={`w-[400px] h-[300px] rounded-lg shadow-xl border border-gray-400 ${active ? "":"hidden"}`} src={image_url} alt="loading" />
+  </>
+  )
+}
+
+
 
 export default App;
